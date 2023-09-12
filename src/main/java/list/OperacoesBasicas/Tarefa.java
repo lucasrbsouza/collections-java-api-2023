553a128485be97dff7d7717c5d8c43f6c86bca17
lucasrbsouza <joselucastrbssilvasouza@gmail.com>
2023-09-12 19:40:24 -0300

    Initial commit

---
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,16 @@
+package main.java.list.OperacoesBasicas;
+
+public class Tarefa {
+    //Atributo
+    private String descricao;
+    public Tarefa (String descricao){
+        this.descricao = descricao;
+    }
+    public String getDescricao(){
+        return descricao;
+    }
+    public String toString(){
+        return "Tarefa{" + "descrição='" + descricao + "\"" + "}";
+    }
+
+}
